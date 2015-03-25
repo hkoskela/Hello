817a3d921e93f3f5f5d5effcf55dd5a24f9f05f8
Henry Koskela <henry.koskela@gmail.com>
2015-03-25 14:07:18 +0200

    modified hello.erl and added checkgit

--- hello.erl
+++ hello.erl
@@ -1,10 +1,23 @@
 -module(hello).
--export([output/0]).
+-export([incr/1,output/0,loop/1]).
+-vsn(1.8).
+
+incr(N) ->
+	N + 1.
 
 output() ->
-	code:purge(?MODULE),
-	timer:sleep(5000),
+	N = 0,
+	loop(N).
+    
+loop(N) ->	
+	code:soft_purge(?MODULE),
+    timer:sleep(3000),
 	code:load_file(?MODULE),
-	io:format("Hep"),
-	?MODULE:output().
+    timer:sleep(2000),
+	V = ?MODULE:module_info(attributes),
+	New = ?MODULE:incr(N),
+	T = calendar:now_to_local_time(now()),
+    Message = 1,
+	io:format("~p ~p Ajettu ~p kertaa: ~p ~n", [T,V,New,Message]),
+    ?MODULE:loop(New).
 
