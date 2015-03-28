22fcc9042aec719dea2dac458b8578ae5ed43191
Henry Koskela <henry.koskela@gmail.com>
2015-03-28 14:30:05 +0200

    changed

--- hello.erl
+++ hello.erl
@@ -1,9 +1,9 @@
 -module(hello).
 -export([start/0,incr/1,output/0,loop/1]).
--vsn(2.0).
+-vsn(2.0.1).
 
 start() ->
-	Pid = spawn(?MODULE,output,[]).
+	spawn(?MODULE,output,[]).
 
 incr(N) ->
 	N + 1.
@@ -14,13 +14,13 @@ output() ->
     
 loop(N) ->	
 	code:soft_purge(?MODULE),
-    timer:sleep(3000),
+    timer:sleep(1000),
 	code:load_file(?MODULE),
-    timer:sleep(2000),
+    timer:sleep(5000),
 	V = ?MODULE:module_info(attributes),
 	New = ?MODULE:incr(N),
 	T = calendar:now_to_local_time(now()),
-    Message = 5,
+    Message = 1,
 	io:format("~p ~p Ajettu ~p kertaa: ~p ~n", [T,V,New,Message]),
     ?MODULE:loop(New).
 
