8216547ea7ec9e787f39fb5e766485998ee575ca
Henry Koskela <henry.koskela@gmail.com>
2015-03-29 21:07:25 +0300

    1

--- hello.erl
+++ hello.erl
@@ -1,6 +1,6 @@
 -module(hello).
 -export([start/0,incr/1,init/0,loop/1,update/0]).
--vsn(2.28).
+-vsn(2.29).
 
 start() ->
 	spawn(?MODULE,init,[]).
@@ -10,10 +10,8 @@ incr(N) ->
 
 update() ->
 	code:purge(?MODULE),
-	timer:sleep(1000),
-	code:load_file(?MODULE),
-	timer:sleep(2000).
-
+	code:load_file(?MODULE).
+	
 init() ->
 	N = 0,
 	loop(N).
@@ -23,7 +21,7 @@ loop(N) ->
 	{_,{_,[V]}}= beam_lib:version(?MODULE),
 	New = ?MODULE:incr(N),
 	T = calendar:now_to_local_time(now()),
-    Message = "ruokaa",
+    Message = "1",
 	io:format("~p Versio: ~p Ajettu ~p kertaa ~p ~n", [T,V,New,Message]),
 	timer:sleep(5000),
     ?MODULE:loop(New).
