695bc6b577d26e18748b1199f1a1832e7bae3c66
Henry Koskela <henry.koskela@gmail.com>
2015-03-30 16:11:09 +0300

    8

--- hello.erl
+++ hello.erl
@@ -1,6 +1,6 @@
 -module(hello).
 -export([start/0,incr/1,init/0,loop/1,update/0]).
--vsn(2.48).
+-vsn(2.49).
 
 start() ->
 	spawn(?MODULE,init,[]).
@@ -22,7 +22,7 @@ loop(N) ->
 	New = ?MODULE:incr(N),
 	%%T = calendar:now_to_local_time(now()),
     T = time(),
-	Message = "1",
+	Message = "5",
 	io:format("~p Versio: ~p Ajettu ~p kertaa ~p ~n", [T,V,New,Message]),
 	timer:sleep(5000),
     ?MODULE:loop(New).
