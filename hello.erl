559501f03c8b61853dc895b96c99fdfeab1d58cf
Henry Koskela <henry.koskela@gmail.com>
2015-03-30 20:01:59 +0300

    8

--- hello.erl
+++ hello.erl
@@ -1,6 +1,6 @@
 -module(hello).
 -export([start/0,incr/1,init/0,loop/1,update/0]).
--vsn(2.57).
+-vsn(2.58).
 
 start() ->
 	spawn(?MODULE,init,[]).
@@ -21,7 +21,7 @@ loop(N) ->
 	{ok,{hello,[V]}} = beam_lib:version(hello),
 	New = ?MODULE:incr(N),
 	{H,M,S} = time(),
-	Message = "8",
+	Message = "Top Chef",
 	io:format("(~p:~p:~p) Versio: ~p Ajettu ~p kertaa ~p ~n", [H,M,S,V,New,Message]),
 	timer:sleep(5000),
     ?MODULE:loop(New).
