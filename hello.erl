1f607c8e0fcfa21a8f2d6352020f9a74aa4802a2
hkoskela <henry.koskela@gmail.com>
2015-04-07 21:09:33 +0300

    virhe

--- hello.erl
+++ hello.erl
@@ -1,6 +1,6 @@
 -module(hello).
 -export([start/0,incr/1,init/0,loop/1,update/0]).
--vsn(2.68).
+-vsn(2.69).
 
 start() ->
 	spawn(?MODULE,init,[]).
@@ -24,5 +24,5 @@ loop(N) ->
 	{ok,{hello,[V]}} = beam_lib:version(hello),
 	io:format("(~p:~p:~p) Versio: ~p Ajettu ~p kertaa ~p ~n", [H,M,S,V,New,Message]),
 	timer:sleep(5000),
-    ?MODULE:loop(New).
+    ?MODULE:loop(New)
 
